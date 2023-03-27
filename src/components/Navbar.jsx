f89bee7aed01d75aa62c20ddebff3ffbb4d87933
kingshuk <kingshukrox@gmail.com>
2023-03-27 19:44:39 +0530

    initial prject structure and page rputing setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Navbar as NavBs } from "react-bootstrap";
+
+export function Navbar() {
+    return (
+        <NavBs className="bg-white shadow-sm mb-3">
+            <Container>
+                hi
+            </Container>
+        </NavBs>
+    );
+}
