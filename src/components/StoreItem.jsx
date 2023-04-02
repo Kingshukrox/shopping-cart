6595310869c6e2f5e102d73a17dd72ce1e11f17d
kingshuk <kingshukrox@gmail.com>
2023-04-02 19:40:45 +0530

    store layout complete with hardcoded quantity values

--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import { currencyFormat } from "../utilities/currencyFormat";
 
 export function StoreItem({ id, name, price, imgUrl,alt }) {
+    const quantity=1;
     return (
         <div>
             <article>
@@ -19,11 +21,21 @@ export function StoreItem({ id, name, price, imgUrl,alt }) {
                             <span className="fs-2">{name}</span>
                             {/*ms-2 : margin-start 2 to prevent title and price from overlapping */}
                             <span className="ms-2 text-secondary">
-                                {price}
+                                {currencyFormat(price)}
                             </span>
                         </Card.Title>
                         <Card.Text>Some text</Card.Text>
-                        <Button variant="primary">Click</Button>
+                        {quantity===0?(<Button variant="primary">+ Add to Cart</Button>):<div className="d-flex flex-column align-items-center" style={{gap:"1rem"}}>
+                                <div className="d-flex" style={{gap:"1rem"}}>
+                                    <Button>-</Button>
+                                    <div>
+                                        <span className="fs-3">{quantity} </span>
+                                        in cart
+                                    </div>
+                                    <Button>+</Button>
+                                </div>
+                                <Button variant="danger">Remove</Button>
+                            </div>}
                     </Card.Body>
                 </Card>
             </article>
